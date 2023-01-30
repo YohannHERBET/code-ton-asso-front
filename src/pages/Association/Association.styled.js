3025a5ac3d55f38d856f518bc6aff6f07d779e6a
YohannHERBET <jukaxyyo@gmail.com>
2023-01-30 16:25:23 +0100

    initialization

--- src/pages/Association/Association.styled.js
+++ src/pages/Association/Association.styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const StyledAssociation = styled.div`
   width: 100vw;
--- src/pages/Error404/Error404.styled.js
+++ src/pages/Error404/Error404.styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const StyledError404 = styled.div`
   width: 100vw;
