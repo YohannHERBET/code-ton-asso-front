bf06a7a75ec016f0be09a4683aead6f86139050b
benjaminrusso-oclock <russobenjamin45@gmail.com>
2023-01-31 10:06:41 +0100

    initialisation

--- src/global/breakpoints.js
+++ src/global/breakpoints.js
@@ -1,10 +1,11 @@
 const breakpoints = {
-  mobileM: '@media (min-width:375px)',
-  mobileL: '@media (min-width:425px)',
-  tablet: '@media (min-width:768px)',
-  laptop: '@media (min-width:1024px)',
-  laptopL: '@media (min-width:1440px)',
-  desktop: '@media (min-width:1920px)',
+  mobileS: '(min-width:320px)',
+  mobileM: '(min-width:375px)',
+  mobileL: '(min-width:425px)',
+  tablet: '(min-width:768px)',
+  laptop: '(min-width:1024px)',
+  laptopL: '(min-width:1440px)',
+  desktop: '(min-width:1920px)',
 };
 
 export default breakpoints;
